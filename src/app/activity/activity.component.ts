03ce54519c5eee06b712cf90faa7e1db1952dfb3
sai varun tangalla <tangallasaivarun6@gmail.com>
2020-01-28 09:55:13 -0500

    implementation of ng-switch directive

---
+++ src/app/activity/activity.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity',
+  templateUrl: './activity.component.html',
+  styleUrls: ['./activity.component.css']
+})
+export class ActivityComponent implements OnInit {
+
+  activities: any;
+  constructor(private http:HttpClient) { 
+
+    
+  }
+
+  ngOnInit() {
+
+    let response= this.http.get("https://demo4690221.mockable.io/getActivities");
+    response.subscribe((data) => this.activities = data);
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivityComponent } from './activity/activity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
